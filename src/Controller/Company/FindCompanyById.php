2a889b6dfedadd584a6bbaf9acc8ef1d5c7223fd
Mehdi <mehdifathi.developer@gmail.com>
2024-02-08 10:38:56 +0330

    Feat:implement find company id end-point

---
+++ src/Controller/Company/FindCompanyById.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller\Company;
+
+use App\Response\ApiResponse;
+use App\Serializer\ApiResponseSerializer;
+use App\Service\CompanyService;
+use App\Service\UserContextInterface;
+use App\Service\UserService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Serializer\SerializerInterface;
+
+#[AsController]
+class FindCompanyById extends AbstractController
+{
+
+    public function __construct(
+        private CompanyService       $companyService,
+        private SerializerInterface  $serializer,
+        private UserContextInterface $userContext
+    )
+    {
+    }
+
+    /**
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function __invoke(int $id)
+    {
+        $company = $this->companyService->findByCompanyId(
+            $id,
+        );
+
+        $res = ApiResponse::getResponse(true, '', $company);
+
+        $serializedData = $this->serializer->serialize($res, 'json', [
+            'groups' => 'get',
+        ]);
+
+        return new JsonResponse($serializedData, 200, [], true);
+
+    }
+}
---
+++ src/Service/CompanyService.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Company;
+use App\Entity\Enum\RoleTypeEnum;
+use App\Entity\User;
+use App\Repository\CompanyRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+/**
+ *
+ */
+final class CompanyService
+{
+
+    /**
+     * @param \App\Repository\CompanyRepository $companyRepository
+     */
+    public function __construct(private CompanyRepository $companyRepository)
+    {
+    }
+
+    /**
+     * @param int $userId
+     * @return \App\Entity\User
+     */
+    public function findUserById(int $userId): User
+    {
+        $user = $this->companyRepository->find($userId);
+
+        return $user;
+    }
+
+    /**
+     * @param int $companyId
+     * @return \App\Entity\Company
+     */
+    public function findByCompanyId(int $companyId): Company
+    {
+        $companyData = $this->companyRepository->find($companyId);
+
+        return $companyData;
+    }
+
+    /**
+     * @param string $name
+     * @param int $companyId
+     * @param string $role
+     * @return void
+     */
+    public function save(string $name, int $companyId, string $role)
+    {
+        $this->companyRepository->create($name, $companyId, $role);
+    }
+
+    /**
+     * @param $id
+     * @return bool
+     */
+    public function delete($id): bool
+    {
+        return $this->companyRepository->delete($id);
+    }
+}
