a59b384a14846694591e69968700a7750f82d165
Mehdi <mehdifathi.developer@gmail.com>
2024-02-09 10:04:55 +0330

    Update:update migration

--- migrations/Version20240208075405.php
+++ migrations/Version20240208075405.php
@@ -20,19 +20,16 @@ final class Version20240208075405 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE "companies_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE "users_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
+        // Check if public schema exists
+        // $this->addSql('CREATE SCHEMA IF NOT EXISTS public;');
         $this->addSql('CREATE TABLE "companies" (id SERIAL NOT NULL, name VARCHAR(100) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE TABLE "users" (id SERIAL NOT NULL, name VARCHAR(100) NOT NULL, role public."role_user" NOT NULL, company_id INT NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('COMMENT ON COLUMN "users".role IS \'(DC2Type:role_user)\'');
+        $this->addSql('CREATE TABLE "users" (id SERIAL NOT NULL, name VARCHAR(100) NOT NULL, role VARCHAR(50) NOT NULL, company_id INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('create index users_company_id_index on users (company_id);');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE "companies_id_seq" CASCADE');
-        $this->addSql('DROP SEQUENCE "users_id_seq" CASCADE');
         $this->addSql('DROP TABLE "companies"');
         $this->addSql('DROP TABLE "users"');
     }
